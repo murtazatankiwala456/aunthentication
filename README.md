e1956ae620d82176d677558b83951f1cf0482937
Murtaza Tankiwala <115138620+murtazatankiwala456@users.noreply.github.com>
2024-05-05 11:06:32 +0530

    L3 hash with md5 and add local host to env var

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-#Athentication From Level 1 to Level 6
+# Athentication 
+From Level 1 to Level 6
--- app.js
+++ app.js
@@ -1,67 +1,62 @@
-import 'dotenv/config'
+import "dotenv/config";
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose, { Schema } from "mongoose";
-import encrypt from "mongoose-encryption"
 
+import md5 from "md5";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://127.0.0.1:27017/userDB');
+mongoose.connect(process.env.LOCAL_HOST);
 
 const userSchema = new mongoose.Schema({
-    email: String,
-    password:String
+  email: String,
+  password: String,
 });
 
+const User = mongoose.model("User", userSchema);
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields: ['password']} );
-
-const User = mongoose.model("User", userSchema)
-
-app.get("/", (req,res) =>{
-    res.render("home.ejs")
+app.get("/", (req, res) => {
+  res.render("home.ejs");
 });
-app.get("/login", (req,res) =>{
-    res.render("login.ejs")
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
 });
-app.get("/register", (req,res) =>{
-    res. render("register.ejs")
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
 });
 
-
-app.post("/register", (req,res) =>{
-    const newUser = new User({
-        email:req.body.username,
-        password: req.body.password
-    });
-    newUser.save();
-    res.render("secrets.ejs")
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: md5(req.body.password),
+  });
+  newUser.save();
+  res.render("secrets.ejs");
 });
 
-app.post("/login",async (req,res) =>{
-    try{
-        const username = req.body.username;
-        const password = req.body.password
+app.post("/login", async (req, res) => {
+  try {
+    const username = req.body.username;
+    const password = md5(req.body.password);
 
-        const findUser =await User.findOne({email: username});
-        if(findUser){
-            if(findUser.password === password){
-                res.render("secrets.ejs") }
-        }else{
-            res.send("Incorrect crendentials!")
-        }
-    }catch(error){
-        res.send(error,"User not found")
-
+    const findUser = await User.findOne({ email: username });
+    if (findUser) {
+      if (findUser.password === password) {
+        res.render("secrets.ejs");
+      }
+    } else {
+      res.send("Incorrect crendentials!");
     }
-
+  } catch (error) {
+    res.send(error, "User not found");
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
