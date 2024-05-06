bd78746c3af063a31ddbac2138b8db0c579ba1ab
Murtaza Tankiwala <115138620+murtazatankiwala456@users.noreply.github.com>
2024-05-06 11:48:42 +0530

    L5 auth with passport js

--- app.js
+++ app.js
@@ -2,16 +2,24 @@ import "dotenv/config";
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose, { Schema } from "mongoose";
-import bcrypt from "bcrypt";
-const saltRounds = 10;
-
-import md5 from "md5";
+import session from "express-session";
+import passport from "passport";
+import passportLocalMongoose from "passport-local-mongoose";
 
 const app = express();
 const port = 3000;
-
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect(process.env.LOCAL_HOST);
 
@@ -20,7 +28,14 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
@@ -31,36 +46,54 @@ app.get("/login", (req, res) => {
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
-
-app.post("/register", (req, res) => {
-  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-    const newUser = new User({
-      email: req.body.username,
-      password: hash,
-    });
-    newUser.save();
+app.get("/secrets", (req, res) => {
+  if (req.isAuthenticated()) {
     res.render("secrets.ejs");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout", (req, res) => {
+  req.logout((error) => {
+    if (error) {
+      console.log(error);
+    }
+    res.redirect("/");
   });
 });
 
-app.post("/login", async (req, res) => {
-  try {
-    const username = req.body.username;
-    const password = req.body.password;
+app.post("/register", (req, res) => {
+  User.register(
+    { username: req.body.username },
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, () => {
+          res.redirect("/secrets");
+        });
+      }
+    }
+  );
+});
 
-    const findUser = await User.findOne({ email: username });
-    if (findUser) {
-      bcrypt.compare(password, findUser.password, (err, result) => {
-        if (result === true) {
-          res.render("secrets.ejs");
-        } else {
-          res.send("Incorrect credentials!");
-        }
+app.post("/login", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  req.login(newUser, (error) => {
+    if (error) {
+      console.log(error);
+    } else {
+      passport.authenticate("local")(req, res, () => {
+        res.redirect("/secrets");
       });
     }
-  } catch (error) {
-    console.error(error);
-  }
+  });
 });
 
 app.listen(port, () => {
